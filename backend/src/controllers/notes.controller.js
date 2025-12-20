030225f98d2dc672f0b0fbda42e1dffe382ea91f
ranjit-dey <ranjitdey05265@gmail.com>
2025-12-20 22:23:29 +0530

    sort according to the updation

--- backend/src/controllers/notes.controller.js
+++ backend/src/controllers/notes.controller.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 import Note from '../models/Note.js'
 export const getAllNotes = async (_, res) => {
     try {
-        const notes = await Note.find().sort({ createdAt: -1 }) // -1 will sort desc (newest first)
+        const notes = await Note.find().sort({ updatedAt: -1 }) // -1 will sort desc (newest first)
         res.status(200).json(notes)
     } catch (error) {
         console.error('Error in getAllNotes controller')
